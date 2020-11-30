f6013660aae1d07895f9239428abe562c2fcd505
HuntingBot <41249395+HuntingBot@users.noreply.github.com>
2020-11-30 13:30:32 +0800

    Update construction-arm.py

--- construction-arm.py
+++ construction-arm.py
@@ -1,6 +1,5 @@
 from glife.base import *
 import golly as g
-import random
 
 vacuum = pattern("")
 g.setalgo("HashLife")
@@ -9,18 +8,21 @@ g.autoupdate(True)
 def dfs(depth, current):
 	if depth == 15:
 		current.display("Single-Channel Recipe Search")
+		# Place block at the appropiate place
 		(block (g.getrect()[0]-11, g.getrect()[1]-10) + current).display("Single-Channel Recipe Search")
+		# Filter boring one-glider solutions out
 		if int(g.getpop()) == 9:
 			return
-		bread = g.getrect()[0]
-		sinkship = g.getrect()[1]
+		bread = g.getrect()[0] # old block x coordinate
+		sinkship = g.getrect()[1] # old block y coordinate
 		g.setstep(5)
 		g.step()
 		if int(g.getpop()) == 4:
-			bruh = str(g.getrect()[0]-bread) + str(g.getrect()[1]-sinkship) + ".rle"
+			bruh = str(g.getrect()[0]-bread) + str(g.getrect()[1]-sinkship) + ".rle" # The offset
 			g.reset()
 			g.save(bruh, "rle")
 		return
-	dfs(depth+1, glider + current[44])
-	dfs(depth+1, vacuum + current[44])
-dfs(0, glider)
+	dfs(depth+1, glider + current[44]) # Assume the next glider is present
+	dfs(depth+1, vacuum + current[44]) # Assume the next glider is absent
+# dfs(0, glider)
+dfs(0, pattern("b2o$2o$2bo9$12b2o$11b2o$13bo9$23b2o$22b2o$24bo9$34b2o$33b2o$35bo!"))
