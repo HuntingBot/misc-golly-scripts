22065608f129d788a03f06b47b917e1c97a2b04b
yujh-yujh <63082800+yujh-yujh@users.noreply.github.com>
2020-12-03 20:30:41 +0800

    dymt

--- construction-arm.py
+++ construction-arm.py
@@ -11,24 +11,24 @@ def dfs(depth, current):
 	(block (g.getrect()[0]-11, g.getrect()[1]-10) + current).display("Single-Channel Recipe Search")
 	# Filter boring one-glider solutions out
 	if int(g.getpop()) == 9:
-		return
+		return False
 	bread = g.getrect()[0] # old block x coordinate
 	sinkship = g.getrect()[1] # old block y coordinate
 	g.run(44)
 	if g.empty():
 		# Use g.empty not g.getpop
 		# Big speedup
-		return
+		return False
 	g.setstep(5)
 	g.step()
 	if g.getrect()[0] - bread > 100 or g.getrect()[1] - sinkship > 100:
-		return # Sorry
+		return False # 
 	if depth == 22:
 		if int(g.getpop()) == 4:
 			bruh = str(g.getrect()[0]-bread) + str(g.getrect()[1]-sinkship) + ".rle" # The offset
 			g.reset()
 			g.save(bruh, "rle")
-		return
+		return True
 	dfs(depth+1, glider + current[44]) # Assume the next glider is present
 	dfs(depth+1, vacuum + current[44]) # Assume the next glider is absent
 # dfs(0, glider)
